6d1ca21634749181e69ba238633e6c37c32feabc
HuyNguyen260398 <huynguyen260398@gmail.com>
2020-07-26 21:51:36 +0700

    Changed template design.

--- src/GEL/views.py
+++ src/GEL/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from post.models import *
 
 
 def home(request):
-    context = {}
+    posts = Post.objects.all().order_by('-created_at')[:4]
+    university_posts = PostTag.objects.filter(
+        tag_id__name='University').order_by('-post_id__created_at')[:3]
+    context = {
+        'posts': posts,
+        'university_posts': university_posts
+    }
     return render(request, "home.html", context)
