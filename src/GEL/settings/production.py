7455993945af58b316f59da06f0396de6fc0580a
HuyNguyen260398 <huynguyen260398@gmail.com>
2020-08-24 21:54:04 +0700

    updated production.py removed django heroku

--- src/GEL/settings/production.py
+++ src/GEL/settings/production.py
@@ -1,28 +1,14 @@
 import os
-import django_heroku
+# import django_heroku
 import dj_database_url
 import environ
-
-# env = environ.Env()
-
-# reading .env file
-# environ.Env.read_env(env.str('./', '.env'))
 
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 
-# False if not in os.environ
 DEBUG = False
 
-# Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
 SECRET_KEY = os.getenv('SECRET_KEY')
-
-# CACHES = {
-#     # read os.environ['CACHE_URL'] and raises ImproperlyConfigured exception if not found
-#     'default': env.cache(),
-#     # read os.environ['REDIS_URL']
-#     'redis': env.cache('REDIS_URL')
-# }
 
 ALLOWED_HOSTS = ['green-edu-link.herokuapp.com']
 
@@ -48,35 +34,6 @@ EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 ADMINS = [('Huy', 'huynguyen260398@gmail.com')]
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'formatters': {
-#         'simple': {
-#             'format': '{levelname} {message}',
-#             'style': '{',
-#         },
-#     },
-#     'handlers': {
-#         'file': {
-#             'level': 'INFO',
-#             'class': 'logging.FileHandler',
-#             'filename': 'D:/Workspace/GreenEduLink/Backend/GreenEduLink/src/debug.log',
-#         },
-#         'mail_admins': {
-#             'level': 'ERROR',
-#             'class': 'django.utils.log.AdminEmailHandler',
-#         }
-#     },
-#     'loggers': {
-#         'django': {
-#             'handlers': ['file', 'mail_admins'],
-#             'propagate': True,
-#         },
-#     }
-# }
-
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 USE_S3 = os.getenv('USE_S3') == 'TRUE'
 
@@ -100,6 +57,3 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/mediafiles/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
-
-
-django_heroku.settings(locals())
