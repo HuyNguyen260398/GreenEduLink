2149cadb513b1953bd28a7a67098e78f2e0601a1
HuyNguyen260398 <huynguyen260398@gmail.com>
2020-09-28 21:32:27 +0700

    Updated features branch

--- src/gallery/views.py
+++ src/gallery/views.py
@@ -7,35 +7,28 @@ from .models import *
 
 
 def gallery(request):
-    pictures = Picture.objects.all().order_by('-timestamp')
-    picture_section = pictures[0]
-    video_section = pictures[1]
-    context = {
-        'picture_gallery': picture_section,
-        'video_gallery': video_section,
-    }
-    return render(request, 'gallery/gallery.html', context)
+    return render(request, 'gallery/gallery.html')
 
 
 def picture_gallery(request):
     pictures = Picture.objects.all().order_by('-timestamp')
-    paginator = Paginator(pictures, 9)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {
-        'pictures': pictures,
-        'page_obj': page_obj
-    }
+    context = {}
+    if pictures.exists():
+        paginator = Paginator(pictures, 9)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context['pictures'] = pictures
+        context['page_obj'] = page_obj
     return render(request, 'gallery/picture_gallery.html', context)
 
 
 def video_gallery(request):
     videos = Video.objects.all().order_by('-timestamp')
-    paginator = Paginator(videos, 9)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {
-        'videos': videos,
-        'page_obj': page_obj
-    }
+    context = {}
+    if videos.exists():
+        paginator = Paginator(videos, 9)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context['videos'] = videos,
+        context['page_obj'] = page_obj
     return render(request, 'gallery/video_gallery.html', context)
